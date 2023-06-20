a2b781c803368eaa5ee29be3ad8a61db523d31a5
Mariano Forti <mariano.forti@icams.rub.de>
2023-06-20 22:32:59 +0200

    trying to get total mapping

--- Tools/__init__.py
+++ Tools/__init__.py
@@ -1,2 +1,20 @@
-from .SchemaUpdater.SchemaUpdater import SchemaUpdater
+import os
+import sys
+import json
+
+from Tools.SchemaUpdater import SchemaUpdater
+from Tools.lis2json import LisParse
+from Tools.Metainfo import metainfo
+
+
+schema_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'Data Schema IUC02/schema_creep_test.json')
+
+def load_schema(
+        filename = schema_file 
+        ):
+
+    with open(schema_file, 'r') as f:
+        schema = json.load(f)
+
+    return schema
 
