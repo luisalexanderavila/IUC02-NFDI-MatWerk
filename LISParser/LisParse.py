9b7e6233e0a0cd1a7f7546f15df1f0cbab75e451
Mariano Forti <marianodforti@gmail.com>
2025-01-22 22:34:20 +0100

    logging, updated lis format

--- LISParser/LisParse.py
+++ LISParser/LisParse.py
@@ -4,6 +4,11 @@ import pdb
 import json
 import re
 import numpy as np
+import pdb
+import logging
+from tqdm.auto import tqdm
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 
 
 def get_key_value(key_value_line: str):
@@ -36,25 +41,34 @@ class Parser():
         for ln, line in enumerate(raw_file_content[2:]):
             if len(line) < 1:
                 continue
-            if line[0] == '-' and len(json_content['metadata']) > 0:
+            if  ( len(line.strip()) == 0 ):
+                continue
+            if ( '[data]' in line ):
                 break
             key, value = get_key_value(line)
+            logger.info(f'key: {key}, value: {value}')
 
             json_content['metadata'][key] = value
 
-        for ln2, line in enumerate(raw_file_content[ln+2:]):
-            if '[Daten]' in line:
-                break
-        data_titles = raw_file_content[ln+ln2+2+1].split()
-        data_units = raw_file_content[ln+ln2+2+2].split()
-        json_content['data'][data_titles[0]] = {'unit' : data_units[0], 'values' : []}
-        json_content['data'][data_titles[1]] = {'unit' : data_units[1], 'values' : []}
-        for ln3, line in enumerate(raw_file_content[ln+ln2+2+2+3:]):
-            if len(line) == 0:
-                continue
-            x, y = np.fromstring(line.replace(',','.'), sep='\t', )
-            json_content['data'][data_titles[0]]['values'].append(x)
-            json_content['data'][data_titles[1]]['values'].append(y)
+        logger.info(f'[data] data starts at line {ln+2}')
+
+        
+        progress = tqdm(enumerate(raw_file_content[ln+3:]), total=len(raw_file_content[ln+3:]))
+        for ln2, line in progress:
+            data_titles = raw_file_content[ln+3+ln2].split('\t')
+            data_units = raw_file_content[ln+3+ln2+2].split('\t')
+            if len(data_titles) != len(data_units):
+                logger.error(f'Error: data_titles and data_units have different lengths')
+            for title, unit in zip(data_titles, data_units):
+                json_content['data'][title] = {'unit' : unit, 'values' : []}
+            for ln3, line in enumerate(raw_file_content[ln+ln2+2+2+3:]):
+                if len(line) == 0:
+                    continue
+                values = np.fromstring(line.replace(',','.'), sep='\t', )
+                for title, value in zip(data_titles, values):
+                    json_content['data'][title]['values'].append(value)
+#                json_content['data'][data_titles[0]]['values'].append(x)
+#                json_content['data'][data_titles[1]]['values'].append(y)
         return json_content
 
 
