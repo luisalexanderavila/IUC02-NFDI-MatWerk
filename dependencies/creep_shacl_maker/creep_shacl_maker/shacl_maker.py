9a632308bc976634ab1ca1c9b23c7273160fcc55
Mariano Forti <mariano.forti@icams.rub.de>
2025-02-05 19:39:54 +0100

    tool fo brute force graph translator

---
+++ dependencies/creep_shacl_maker/creep_shacl_maker/shacl_maker.py
@@ -0,0 +1,106 @@
+import json
+import os
+import sys
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(os.path.basename(__file__))
+
+
+header_file  = os.path.join(os.path.dirname(__file__),  'shaclShapes_Graph_small_exampl.ttl')
+
+with open(header_file, 'r') as head:
+    header = head.readlines()
+
+def get_tesStandardParagraph(thetestandard):
+    paragraph_id = '_'.join(thetestandard.split()) 
+    paragraph = [
+        f":{paragraph_id} :TestStandard;\n",
+        f":testStandardApplied \"True\"^^xsd:boolean ;\n",
+        f":testStandard \"{thetestandard}\"^^xsd:string.\n",
+        ]
+    return paragraph 
+
+def get_initalStressParagraph(theInitialStress):
+    if 'value' not in theInitialStress:
+        logger.error('initialStress value is missing')
+        sys.exit(1)
+    if 'unit' not in theInitialStress:
+        logger.error('initialStress unit is missing')
+        sys.exit(1)
+
+    try:
+        initialstress_value = float(theInitialStress['value'])
+    except ValueError as E:
+        logger.error('initialStress is not a float')
+    initialstress_unit = theInitialStress['unit']
+
+    paragraph_id = f'InitialStress_{initialstress_value}_{initialstress_unit}'
+    paragraph = [
+    f':{paragraph_id} a :Quality;\n',
+    f'  :hasSpecifiedNumericValue \"{initialstress_value}\"^^xsd:float;\n'
+    f'  :hasUnit \"{initialstress_unit}\"^^xsd:string.\n'
+    ]
+    return paragraph
+
+def write_shacl_metadata(json_metadata):
+    parent = json_metadata['testInfo']['testParameters']#['materialHistoryAndConditions']
+    if 'testStandard' in parent:
+        teststandard = parent['testStandard']
+        if not isinstance(teststandard, str):
+            logger.error(' testStandard is not a string ')
+            sys.exit(1)
+        test_standard_paragraph = get_tesStandardParagraph(teststandard)
+
+    parent = json_metadata['testInfo']['testParameters']
+    if 'initialStress' in parent:
+        initialStress = parent['initialStress']
+        initialStress_paragraph = get_initalStressParagraph(initialStress)
+
+    return test_standard_paragraph+['\n']+initialStress_paragraph
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_json', type=str, help='input json file with  creep test metadata')
+    parser.add_argument('-o', '--output', type=str, dest='output_ttl',  help='output shacl file', default=None)
+    args = parser.parse_args()
+    if args.output_ttl is None:
+        args.output_ttl = os.path.splitext(args.input_json)[0] + '.ttl'
+    
+    logger.info(f'input json file: {args.input_json}')
+    logger.info(f'output json file: {args.output_ttl}')
+
+    with open(args.input_json, 'r') as f:
+        json_metadata = json.load(f)
+    logger.info(f'keys : {json_metadata.keys()}')
+    json_metadata = json_metadata['mappedMeasurementData']["MeasurementData"]["additionalMetadata"]
+
+    shacl_metadata = write_shacl_metadata(json_metadata)
+
+    with open(args.output_ttl, 'w') as f:
+        f.writelines(header)
+        f.writelines(shacl_metadata)
+
+		
+
+
+    #:CMSX-6 a :TestedMaterial;
+    #    :digitalMaterialIdentifier "CMSX-6"^^xsd:string.
+    #
+    #:SpecifiedTemperature a :Quality;	
+    #        :hasSpecifiedNumericValue "980"^^xsd:float;
+    #        :hasUnit "°C"^^xsd:string.	
+    #
+    #:TypeOfLoading :typeOfLoading "Tension"^^xsd:string.
+    #
+    #:DescriptionOfManufacturingProcess :hasDescription "Description of the manufacturing process - as-tested material. Single Crystal Investment Casting from a Vacuum Induction Refined Ingot and subsequent Heat Treatment (annealed and aged)."^^xsd:string.
+    #
+    #
+    #:TestJob :TestID "Vh5205_C-78"^^xsd:string.
+    #
+    #
+    #:TestJob :dateOfTestStart "2023-08-02T09:06"^^xsd:dateTime.
+    #
+
